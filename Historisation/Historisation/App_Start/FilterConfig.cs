2c3c61a027bd84954c611fa4d09d4e24a36104f8
petrakrat <petra.krat@hotmail.com>
2014-10-25 14:26:42 +0200

    Création du projet
    Éléments de travail associés : #2

---
+++ Historisation/Historisation/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Historisation
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Historisation/Historisation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Historisation.MvcApplication" Language="C#" %>
