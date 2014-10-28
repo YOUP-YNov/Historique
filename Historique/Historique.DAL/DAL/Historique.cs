eec7f12875c0147dc3e3c958b47313031571ecab
petrakrat <petra.krat@hotmail.com>
2014-10-28 18:26:48 +0100

    Signed-off-by: petrakrat <petra.krat@hotmail.com>

---
+++ Historique/Historique.DAL/DAL/Historique.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Historique.DAL.DAO;
+
+namespace Historique.DAL.DAL
+{
+    public partial class Historique
+    {
+        #region Properties
+
+        private HistoriqueTableAdapters.UT_UtilisateurTableAdapter _UserService;
+
+        public HistoriqueTableAdapters.UT_UtilisateurTableAdapter UserService
+        {
+            get
+            {
+                if (_UserService == null)
+                    _UserService = new HistoriqueTableAdapters.UT_UtilisateurTableAdapter();
+                return _UserService;
+            }
+            set
+            {
+                _UserService = value;
+            }
+        }
+
+        private HistoriqueTableAdapters.ps_GetAllEvenementTableAdapter _EvenementService;
+
+        public HistoriqueTableAdapters.ps_GetAllEvenementTableAdapter EvenementService
+        {
+            get
+            {
+                if (_EvenementService == null)
+                    _EvenementService = new HistoriqueTableAdapters.ps_GetAllEvenementTableAdapter();
+                return _EvenementService;
+            }
+            set
+            {
+                _EvenementService = value;
+            }
+        }
+
+        private HistoriqueTableAdapters.UT_CategorieTableAdapter _CategorieService;
+
+        public HistoriqueTableAdapters.UT_CategorieTableAdapter CategorieService
+        {
+            get
+            {
+                if (_CategorieService == null)
+                    _CategorieService = new HistoriqueTableAdapters.UT_CategorieTableAdapter();
+                return _CategorieService;
+            }
+            set
+            {
+                _CategorieService = value;
+            }
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        public IEnumerable<CategorieDao> GetAllCategorie()
+        {
+            var cat = CategorieService.GetData();
+            List<CategorieDao> catDaoList = (List<CategorieDao>)cat.ToDaoCategories();
+            return catDaoList;
+        }
+
+        public IEnumerable<UtilisateurDao> GetAllUser()
+        {
+            var users = UserService.GetData();
+            List<UtilisateurDao> userDaoList = (List<UtilisateurDao>)users.ToDaoUtilisateurs();
+
+            foreach (var user in userDaoList)
+            {
+                var nbAmis = UserService.NbAmisByPseudo(user.Pseudo);
+                var nbEventPropose = UserService.NbEvenementProposeByPseudo(user.Pseudo);
+                var nbEventParticipe = UserService.NbEvenementParticipeByPseudo(user.Pseudo);
+                user.NbAmis = nbAmis.Value;
+                user.NbEvenementParticipe = nbEventParticipe.Value;
+                user.NbEvenmentPropose = nbEventPropose.Value;
+            }
+            return userDaoList;
+        }
+
+        public UtilisateurDao GetUserById(int id)
+        {
+            var nbAmis = UserService.NbAmis(id);
+            var nbEventPropose = Convert.ToInt32(UserService.NbEvenementPropose(id));
+            var nbEventParticipe = Convert.ToInt32(UserService.NbEvenementParticipe(id));
+
+            var result = UserService.GetDataById(id);
+            List<UtilisateurDao> userDaoList = (List<UtilisateurDao>)result.ToDaoUtilisateurs();
+            var userDao = userDaoList.First();
+
+            userDao.NbAmis = nbAmis.Value;
+            userDao.NbEvenementParticipe = nbEventParticipe;
+            userDao.NbEvenmentPropose = nbEventPropose;
+            return userDao;
+        }
+
+        public UtilisateurDao GetUserByPseudo(string pseudo)
+        {
+            var nbAmis = UserService.NbAmisByPseudo(pseudo);
+            var nbEventPropose = UserService.NbEvenementProposeByPseudo(pseudo);
+            var nbEventParticipe = UserService.NbEvenementParticipeByPseudo(pseudo);
+
+            var result = UserService.GetDataByPseudo(pseudo);
+            List<UtilisateurDao> userDaoList = (List<UtilisateurDao>)result.ToDaoUtilisateurs();
+            var userDao = userDaoList.First();
+
+            userDao.NbAmis = nbAmis.Value;
+            userDao.NbEvenementParticipe = nbEventParticipe.Value;
+            userDao.NbEvenmentPropose = nbEventPropose.Value;
+            return userDao;
+        }
+
+        public IEnumerable<EvenementDao> GetAllEvenement()
+        {
+            var evenement = EvenementService.GetData();
+            List<EvenementDao> eventDaoList = (List<EvenementDao>)evenement.ToDaoEvenements();
+            return eventDaoList;
+        }
+
+        public IEnumerable<EvenementDao> GetEvenementByCateByCp(int codePostale, string categorie)
+        {
+            var evenement = EvenementService.GetDataByCateByCp(codePostale, categorie);
+            List<EvenementDao> eventDaoList = (List<EvenementDao>)evenement.ToDaoEvenements();
+            return eventDaoList;
+        }
+
+        public IEnumerable<EvenementDao> GetEvenementByCp(int codePostale)
+        {
+            var evenement = EvenementService.GetDataByCp(codePostale);
+            List<EvenementDao> eventDaoList = (List<EvenementDao>)evenement.ToDaoEvenements();
+            return eventDaoList;
+        }
+
+        #endregion Methods
+    }
+}
---
+++ Historique/Historique.DAL/DAO/UtilisateurDao.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Historique.DAL.DAO
+{
+    public class UtilisateurDao
+    {
+        private int _Id;
+
+        public int Id
+        {
+            get { return _Id; }
+            set { _Id = value; }
+        }
+
+        private string _Pseudo;
+
+        public string Pseudo
+        {
+            get { return _Pseudo; }
+            set { _Pseudo = value; }
+        }
+
+        private DateTime _DateInscription;
+
+        public DateTime DateInscription
+        {
+            get { return _DateInscription; }
+            set { _DateInscription = value; }
+        }
+
+        private string _CheminPhoto;
+
+        public string CheminPhoto
+        {
+            get { return _CheminPhoto; }
+            set { _CheminPhoto = value; }
+        }
+
+        private int _Age;
+
+        public int Age
+        {
+            get { return _Age; }
+            set { _Age = value; }
+        }
+
+        private int _NbAmis;
+
+        public int NbAmis
+        {
+            get { return _NbAmis; }
+            set { _NbAmis = value; }
+        }
+
+        private int _NbEvenementPropose;
+
+        public int NbEvenmentPropose
+        {
+            get { return _NbEvenementPropose; }
+            set { _NbEvenementPropose = value; }
+        }
+
+        private int _NbEvenementParticipe;
+
+        public int NbEvenementParticipe
+        {
+            get { return _NbEvenementParticipe; }
+            set { _NbEvenementParticipe = value; }
+        }
+
+        private string _Ville;
+
+        public string Ville
+        {
+            get { return _Ville; }
+            set { _Ville = value; }
+        }
+
+        private string _CodePostale;
+
+        public string CodePostale
+        {
+            get { return _CodePostale; }
+            set { _CodePostale = value; }
+        }
+    }
+}
