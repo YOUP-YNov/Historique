cbe73df588af2573e229b2dc8e6946d64cad03ee
Ruiz Coralie <ruiz.coralie64@live.fr>
2014-11-02 15:05:28 +0100

    Mise en place du helper (methodes web api) #2
    Éléments de travail associés : #14; #69

---
+++ Historique/Historique/Controllers/EvenementController.cs
@@ -0,0 +1,81 @@
+﻿using Historique.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Historique.Controllers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class EvenementController : ApiController
+    {
+
+        // GET api/evenement
+        /// <summary>
+        /// Retourne tous les évènements disponibles
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Evenement> Get()
+        {
+            return null;
+        }
+
+ 
+        /// <summary>
+        /// Retourne les évènements qui se déroulent durant une saisonalité donnée
+        /// </summary>
+        /// <param name="dateDebut">date debut.</param>
+        /// <param name="dateFin">date fin.</param>
+        /// <returns></returns>
+        public IEnumerable<Evenement> GetEvenementParSaisonalité(DateTime dateDebut, DateTime dateFin)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Retourne les évènements en fonction d'un code postale et d'une catégorie donnés
+        /// </summary>
+        /// <param name="codePostale">code postale.</param>
+        /// <param name="categorie">categorie.</param>
+        /// <returns></returns>
+        public IEnumerable<Evenement> GetEvenementByCateByCp(int codePostale, string categorie)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Retourne les évènements en fonction d'un code postale donné
+        /// </summary>
+        /// <param name="codePostale">code postale.</param>
+        /// <returns></returns>
+        public IEnumerable<Evenement> GetEvenementByCp(int codePostale)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Retourne les évènements en fonction d'une catégorie donnée
+        /// </summary>
+        /// <param name="categorie">categorie.</param>
+        /// <returns></returns>
+        public IEnumerable<Evenement> GetEvenementByCategorie(string categorie)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Retourne les évènements en fonction d'un utilisateur
+        /// </summary>
+        /// <param name="userId">Identifiant de l'utilisateur.</param>
+        /// <returns></returns>
+        public IEnumerable<Evenement> GetEvenementByUtilisateurId(int userId)
+        {
+            return null;
+        }
+
+    }
+}
