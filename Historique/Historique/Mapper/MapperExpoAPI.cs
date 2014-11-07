3a01bf8423edd1ca0d29f198978155765ea11a78
Dyk33 <yohan.dubourg@ingesup.com>
2014-11-07 17:28:45 +0100

    MàJ Automapper toBusiness & ToBusinessList
    Éléments de travail associés : #14; #70

---
+++ Historique/Historique/Mapper/MapperExpoAPI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Historique.Business.Models;
+using Historique.Models;
+
+
+
+
+
+
+
+namespace Historique.Mapper
+{
+    public class MapperExpoAPI
+    {
+        public CategorieBll categorieBll;
+        public EvenementBll evenementBll;
+        public EvenementLieuBll evenementLieuBll;
+        public UtilisateurBll utilisateurBll;
+
+
+
+        public void CreateMapperBll()
+        {
+            AutoMapper.Mapper.CreateMap<CategorieBll,Categorie>(); //définiton du mapping
+            Categorie categorie = AutoMapper.Mapper.Map<CategorieBll, Categorie>(categorieBll);
+
+            //      CategorieBll categorieBll = AutoMapper.Mapper.Map<CategorieDao, CategorieBll>(categorieDao);//Convert
+     
+        }
+
+
+    }
+}
