14946cfd0571c2c5046b1e4555a31dcb0e1bf428
petrakrat <petra.krat@hotmail.com>
2014-11-07 17:53:48 +0100

    Mapper business finission
    Éléments de travail associés : #71

--- Historique/Historique.Business/Mapper/HistoriqueBll.cs
+++ Historique/Historique.Business/Mapper/HistoriqueBll.cs
@@ -22,7 +22,8 @@ namespace Historique.Business.Mapper
             var allcatBll = new List<CategorieBll>();
             try
             {
-                var allcat = _HistoriqueDAL.GetAllCategorie();
+                var allcat = _HistoriqueDAL.GetAllCategorie().ToList();
+                allcatBll = Mapper.ToCategoriesSBll(allcat);
             }
             catch (Exception ex) { }
             return allcatBll;
@@ -33,7 +34,8 @@ namespace Historique.Business.Mapper
             var allusersBll = new List<UtilisateurBll>();
             try
             {
-                var allusers = _HistoriqueDAL.GetAllUser();
+                var allusers = _HistoriqueDAL.GetAllUser().ToList();
+                allusersBll = Mapper.ToUtilisateursBll(allusers);
             }
             catch (Exception ex) { }
             return allusersBll;
@@ -45,6 +47,7 @@ namespace Historique.Business.Mapper
             try
             {
                 var userID = _HistoriqueDAL.GetUserById(id);
+                userIDBll = Mapper.ToUtilisateurBll(userID);
             }
             catch (Exception ex) { }
             return userIDBll;
@@ -56,6 +59,7 @@ namespace Historique.Business.Mapper
             try
             {
                 var userPseudo = _HistoriqueDAL.GetUserByPseudo(pseudo);
+                userPseudoBll = Mapper.ToUtilisateurBll(userPseudo);
             }
             catch (Exception ex) { }
             return userPseudoBll;
@@ -66,7 +70,8 @@ namespace Historique.Business.Mapper
             var evenAllBll = new List<EvenementBll>();
             try
             {
-                var evenAll = _HistoriqueDAL.GetAllEvenement();
+                var evenAll = _HistoriqueDAL.GetAllEvenement().ToList();
+                evenAllBll = Mapper.ToEvenementsBll(evenAll);
             }
             catch (Exception ex) { }
             return evenAllBll;
@@ -77,7 +82,8 @@ namespace Historique.Business.Mapper
             var evenCatCpBll = new List<EvenementBll>();
             try
             {
-                var evenCatCp = _HistoriqueDAL.GetEvenementByCateByCp(codePostale, categorie);
+                var evenCatCp = _HistoriqueDAL.GetEvenementByCateByCp(codePostale, categorie).ToList();
+                evenCatCpBll = Mapper.ToEvenementsBll(evenCatCp);
             }
             catch (Exception ex) { }
             return evenCatCpBll;
@@ -88,7 +94,8 @@ namespace Historique.Business.Mapper
             var evenCpBll = new List<EvenementBll>();
             try
             {
-                var evenCp = _HistoriqueDAL.GetEvenementByCp(codePostale);
+                var evenCp = _HistoriqueDAL.GetEvenementByCp(codePostale).ToList();
+                evenCpBll = Mapper.ToEvenementsBll(evenCp);
             }
             catch (Exception ex) { }
             return evenCpBll;
@@ -99,7 +106,8 @@ namespace Historique.Business.Mapper
             var evenCatBll = new List<EvenementBll>();
             try
             {
-                var evenCat = _HistoriqueDAL.GetEvenementByCat(categorie, dateDebut, dateFin);
+                var evenCat = _HistoriqueDAL.GetEvenementByCat(categorie, dateDebut, dateFin).ToList();
+                evenCatBll = Mapper.ToEvenementsBll(evenCat);
             }
             catch (Exception ex) { }
             return evenCatBll;
@@ -110,7 +118,8 @@ namespace Historique.Business.Mapper
             var evenDateBll = new List<EvenementBll>();
             try
             {
-                var evenDate = _HistoriqueDAL.GetEvenementByDates(dateDebut, dateFin);
+                var evenDate = _HistoriqueDAL.GetEvenementByDates(dateDebut, dateFin).ToList();
+                evenDateBll = Mapper.ToEvenementsBll(evenDate);
             }
             catch (Exception ex) { }
             return evenDateBll;
@@ -121,7 +130,8 @@ namespace Historique.Business.Mapper
             var evenParticipeUserBll = new List<EvenementBll>();
             try
             {
-                var evenParticipeUser = _HistoriqueDAL.GetEvenementParticipeByUserId(userId);
+                var evenParticipeUser = _HistoriqueDAL.GetEvenementParticipeByUserId(userId).ToList();
+                evenParticipeUserBll = Mapper.ToEvenementsBll(evenParticipeUser);
             }
             catch (Exception ex)
             {
@@ -134,7 +144,8 @@ namespace Historique.Business.Mapper
             var userParticipeEvenBll = new List<UtilisateurBll>();
             try
             {
-                var userParticipeEven = _HistoriqueDAL.GetUtilisateurParticipeByEvenementId(eventId);
+                var userParticipeEven = _HistoriqueDAL.GetUtilisateurParticipeByEvenementId(eventId).ToList();
+                userParticipeEvenBll = Mapper.ToUtilisateursBll(userParticipeEven);
             }
             catch (Exception ex)
             {
@@ -147,7 +158,8 @@ namespace Historique.Business.Mapper
             var evenProposeUserBll = new List<EvenementBll>();
             try
             {
-                var evenProposeUser = _HistoriqueDAL.GetEvenementProposeByUserId(userId);
+                var evenProposeUser = _HistoriqueDAL.GetEvenementProposeByUserId(userId).ToList();
+                evenProposeUserBll = Mapper.ToEvenementsBll(evenProposeUser);
             }
             catch (Exception ex)
             {
@@ -161,6 +173,7 @@ namespace Historique.Business.Mapper
             try
             {
                 var userProposeEven = _HistoriqueDAL.GetUtilisateurProposeByEvenementId(eventId);
+                userProposeEvenBll = Mapper.ToUtilisateurBll(userProposeEven);
             }
             catch (Exception ex)
             {
