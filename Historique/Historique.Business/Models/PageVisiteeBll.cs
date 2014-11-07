f1735b03f11429f4e42833d273b56c14c14ce127
Ruiz Coralie <ruiz.coralie64@live.fr>
2014-11-07 18:04:31 +0100

    Mise en place Api Google Analytics #1
    Éléments de travail associés : #9; #34

---
+++ Historique/Historique.Business/Models/PageVisiteeBll.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Historique.Business.Models
+{
+    public class PageVisiteeBll
+    {
+        private string _Nom;
+
+        public string Nom
+        {
+            get { return _Nom; }
+            set { _Nom = value; }
+        }
+
+        private int _NbVue;
+
+        public int NbVue
+        {
+            get { return _NbVue; }
+            set { _NbVue = value; }
+        }
+
+        private double _TempsMoyen;
+
+        public double TempsMoyen
+        {
+            get { return _TempsMoyen; }
+            set { _TempsMoyen = value; }
+        }
+        
+    }
+}
