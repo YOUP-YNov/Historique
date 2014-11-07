396bded64949ea69be8a0207742969ec1b9d65b6
petrakrat <petra.krat@hotmail.com>
2014-11-07 14:25:24 +0100

    Ajout tous les événements pour une date
    Éléments de travail associés : #14; #22

--- Historique/Historique.Business/Mapper/Mapper.cs
+++ Historique/Historique.Business/Mapper/Mapper.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Historique.Business.Models;
 
 namespace Historique.Business.Mapper
 {
     public class Mapper
     {
+       
     }
 }
