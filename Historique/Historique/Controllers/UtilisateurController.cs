46c8f956664710f726ec0b237af9e953c2f94d79
Ruiz Coralie <ruiz.coralie64@live.fr>
2014-11-14 16:29:05 +0100

    UtilisateurController complété + changement de chaine de connexion à la base

--- Historique/Historique/Controllers/UtilisateurController.cs
+++ Historique/Historique/Controllers/UtilisateurController.cs
@@ -4,8 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
+using System.Web;
 
 namespace Historique.Controllers
 {
@@ -33,7 +33,14 @@ namespace Historique.Controllers
         /// <returns></returns>
         public IEnumerable<Utilisateur> GetTopAmis(int top)
         {
-            return null;
+            var utilisateurs = new List<Utilisateur>();
+            var utilisateursAll = Get();
+            if (utilisateursAll != null)
+                utilisateurs = utilisateursAll.OrderByDescending(x => x.NbAmis).ToList();
+
+            utilisateurs= utilisateurs.Take(top).ToList();
+
+            return utilisateurs;
         }
 
 
@@ -44,7 +51,14 @@ namespace Historique.Controllers
         /// <returns></returns>
         public IEnumerable<Utilisateur> GetTopEvenementCree(int top)
         {
-            return null;
+            var utilisateurs = new List<Utilisateur>();
+            var utilisateursAll = Get();
+            if (utilisateursAll != null)
+                utilisateurs = utilisateursAll.OrderByDescending(x => x.NbEvenmentPropose).ToList();
+
+            utilisateurs = utilisateurs.Take(top).ToList();
+
+            return utilisateurs;
         }
 
         /// <summary>
@@ -54,7 +68,14 @@ namespace Historique.Controllers
         /// <returns></returns>
         public IEnumerable<Utilisateur> GetToEvenementParticipe(int top)
         {
-            return null;
+            var utilisateurs = new List<Utilisateur>();
+            var utilisateursAll = Get();
+            if (utilisateursAll != null)
+                utilisateurs = utilisateursAll.OrderByDescending(x => x.NbEvenementParticipe).ToList();
+
+            utilisateurs = utilisateurs.Take(top).ToList();
+
+            return utilisateurs;
         }
 
         /// <summary>
@@ -65,7 +86,15 @@ namespace Historique.Controllers
         /// <returns></returns>
         public IEnumerable<Utilisateur> GetByTrancheAge(int ageDebut, int ageFin)
         {
-            return null;
+            var utilisateurs = new List<Utilisateur>();
+            var utilisateursAll = Get();
+            if (utilisateursAll != null)
+                utilisateurs = utilisateursAll.Where(x => x.Age >= ageDebut && x.Age <= ageFin).ToList();
+
+            if (utilisateurs == null)
+                utilisateurs = new List<Utilisateur>();
+
+            return utilisateurs;
         }
 
         /// <summary>
@@ -75,18 +104,30 @@ namespace Historique.Controllers
         /// <returns></returns>
         public IEnumerable<Utilisateur> GetBySexe(bool homme)
         {
-            return null;
+            var utilisateurs = new List<Utilisateur>();
+            var utilisateursAll = Get();
+            if(utilisateursAll !=null)
+                utilisateurs = utilisateursAll.Where(x => x.IsHomme == homme).ToList();
+
+            if (utilisateurs == null)
+                utilisateurs = new List<Utilisateur>();
+
+            return utilisateurs;
         }
 
-        /// <summary>
-        /// Retourne les utilisateurs participants d'un évènement
-        /// </summary>
-        /// <param name="evenement_Id">l'identifiant de l'évènement.</param>
-        /// <returns></returns>
-        public IEnumerable<Utilisateur> GetParticipants(int evenement_Id)
-        {
-            return null;
-        }
+        ///// <summary>
+        ///// Retourne les utilisateurs participants d'un évènement
+        ///// </summary>
+        ///// <param name="evenement_Id">l'identifiant de l'évènement.</param>
+        ///// <returns></returns>
+        //public IEnumerable<Utilisateur> GetParticipants(int evenement_Id)
+        //{
+        //    var utilisateursAll = Get();
+        //    var utilisateurs = utilisateursAll.SingleOrDefault(x => x.Id.Equals(id));
+        //    if (utilisateurs == null)
+        //        utilisateurs = new List<Utilisateur>();
+        //    return utilisateurs;
+        //}
 
 
         // GET api/utilisateur/5
@@ -95,12 +136,17 @@ namespace Historique.Controllers
         /// </summary>
         /// <param name="id">Indentifiant</param>
         /// <returns></returns>
+        [HttpGet]
         public Utilisateur Get(int id)
         {
-            var utilisateurs = Get();
-            var utilisateur = utilisateurs.SingleOrDefault(x => x.Id.Equals(id));
-            if (utilisateur == null)
-                utilisateur = new Utilisateur();
+            var utilisateur = new Utilisateur();
+            var utilisateursAll = Get();
+            if(utilisateursAll !=null)
+                utilisateur = utilisateursAll.SingleOrDefault(x => x.Id.Equals(id));
+
+            //if (utilisateur == null)
+            //    utilisateur = new Utilisateur();
+
             return utilisateur;
         }
 
