0c78d6fcd65c9992bff270a1d8613566bf973618
Florian Lopes <florian.lopes@outlook.com>
2014-11-16 19:54:30 +0100

    Added /analytics controller + stats by os/deviceCat

--- Historique/Historique.Business/ApiGoogleAnalytics/Analytics.cs
+++ Historique/Historique.Business/ApiGoogleAnalytics/Analytics.cs
@@ -15,8 +15,9 @@ namespace Historique.Business.ApiGoogleAnalytics
             {
             }
 
-            public const string PAGE_PATH = "ga:pagePath";
-
+            public static readonly string PAGE_PATH = "ga:pagePath";
+            public static readonly string OPERATING_SYSTEM = "ga:operatingSystem";
+            public static readonly string DEVICE_CATEGORY = "ga:deviceCategory";
         }
 
         public class Metrics
@@ -25,8 +26,10 @@ namespace Historique.Business.ApiGoogleAnalytics
             {
             }
 
-            public const string PAGE_VIEWS = "ga:pageviews";
-            public const string TIME_ON_PAGE = "ga:timeOnPage";
+            public static readonly string PAGE_VIEWS = "ga:pageviews";
+            public static readonly string TIME_ON_PAGE = "ga:timeOnPage";
+            public static readonly string SESSIONS = "ga:sessions";
+            public static readonly string CONVERSION_RATE = "ga:goalConversionRateAll";
         }
 
         public static string GetCommaSeparatedMetricsString(params string[] metadatas)
--- Historique/Historique.Business/Service/HistoricAnalyticService.cs
+++ Historique/Historique.Business/Service/HistoricAnalyticService.cs
@@ -3,6 +3,7 @@ using System.Configuration;
 using Google.Apis.Analytics.v3.Data;
 using Historique.Business.ApiGoogleAnalytics;
 using Historique.Business.Models;
+using Historique.Business.Models.Analytics;
 using GA = Historique.Business.ApiGoogleAnalytics.Analytics;
 
 namespace Historique.Business.Mapper
@@ -13,17 +14,40 @@ namespace Historique.Business.Mapper
 
         public IEnumerable<PageVisiteeBll> GetPagesVisiteesBll(string startDate, string endDate)
         {
-            var pagesVisiteesBll = new List<PageVisiteeBll>();
-
             var optionalValues = new ReportingHelper.OptionalValues()
             {
                 Dimensions = GA.Dimensions.PAGE_PATH,
             };
             string metrics = GA.GetCommaSeparatedMetricsString(GA.Metrics.PAGE_VIEWS, GA.Metrics.TIME_ON_PAGE);
 
-            GaData gaData = ReportingHelper.Get(AnalyticsTools.GetAnalyticsService(), ANALYTICS_PROFILE_ID, startDate, endDate, metrics, optionalValues);
+            return PageVisiteeBll.FromAnalyticsData(QueryAnalyticsApi(startDate, endDate, metrics, optionalValues));
+        }
 
-            return PageVisiteeBll.FromAnalyticsData(gaData);
+        public IEnumerable<DeviceTypeStatRowBll> GetDevicesStats(string startDate, string endDate)
+        {
+            var optionalValues = new ReportingHelper.OptionalValues()
+            {
+                Dimensions = GA.Dimensions.OPERATING_SYSTEM,
+            };
+            string metrics = GA.GetCommaSeparatedMetricsString(GA.Metrics.SESSIONS, GA.Metrics.CONVERSION_RATE);
+
+            return DeviceTypeStatRowBll.FromAnalyticsData(QueryAnalyticsApi(startDate, endDate, metrics, optionalValues));
+        }
+
+        public IEnumerable<DeviceCategoryStatRowBll> GetDevicesCategoryStats(string startDate, string endDate)
+        {
+            var optionalValues = new ReportingHelper.OptionalValues()
+            {
+                Dimensions = GA.Dimensions.DEVICE_CATEGORY,
+            };
+            string metrics = GA.GetCommaSeparatedMetricsString(GA.Metrics.SESSIONS, GA.Metrics.CONVERSION_RATE);
+
+            return DeviceCategoryStatRowBll.FromAnalyticsData(QueryAnalyticsApi(startDate, endDate, metrics, optionalValues));
+        }
+
+        private GaData QueryAnalyticsApi(string startDate, string endDate, string metrics, ReportingHelper.OptionalValues optionalValues)
+        {
+            return ReportingHelper.Get(AnalyticsTools.GetAnalyticsService(), ANALYTICS_PROFILE_ID, startDate, endDate, metrics, optionalValues);
         }
     }
 }
